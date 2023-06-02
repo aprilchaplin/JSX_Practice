5645b25647fff94c4fe11b199e9aab3066e9caab
aprilchaplin <130192278+aprilchaplin@users.noreply.github.com>
2023-06-02 09:41:27 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>This is my h1</h1>
+    <ul>
+      <li>this is item number 1</li>
+      <li>this is item number 2</li>
+      <li>this is item number 3</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
+
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
